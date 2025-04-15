daebe59ea0bdd28a3bf93a9e33833acc012173ed
BrancoAntuña <118689753+BrancoMartin@users.noreply.github.com>
2025-04-15 09:40:14 -0300

    Branco: creacion de componente Register

---
+++ src/components/Register/Register.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+const Register = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    lastName: "",
+    email: "",
+    password: "",
+    verifyPassword: "",
+  });
+
+  function handleChange(e) {
+    setFormData({ ...formData, [e.target.name]: e.target.values });
+  }
+
+  return (
+    <div>
+      <form action="">
+        <label>Ingrese su nombre:</label>
+        <input name="name" type="text" onChange={handleChange} />
+
+        <label>Ingrese su apellido:</label>
+        <input name="lastName" type="text" onChange={handleChange} />
+
+        <label>Ingrese su email:</label>
+        <input name="email" type="email" onChange={handleChange} />
+
+        <label>Ingrese su contraseña:</label>
+        <input name="password" type="password" onChange={handleChange} />
+
+        <label>Ingrese de nuevo su contraseña:</label>
+        <input name="verifyPassword" type="password" onChange={handleChange} />
+      </form>
+    </div>
+  );
+};
+
+export default Register;
