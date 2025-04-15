32bf39fc3e76102f1eada8ddec23a730c8debbb2
BrancoAntuña <118689753+BrancoMartin@users.noreply.github.com>
2025-04-15 10:00:35 -0300

    Branco: creacion de pages Register

---
+++ src/pages/Register.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Register from "../components/Register/Register";
+
+const Register = () => {
+  return (
+    <div>
+      <Register />
+    </div>
+  );
+};
+
+export default Register;
