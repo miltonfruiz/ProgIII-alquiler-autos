d7ec9b38235c22aea37972118794e469512e4686
Milton Franco Ruiz <miltonfruizok@outlook.es>
2025-04-20 23:40:40 -0300

    Milton 20/04: Modificación archivo Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,9 +2,21 @@ import LoginForm from "../components/Login/LoginForm";
 
 const Login = () => {
   return (
-    <div>
-      <LoginForm />
+    <div className="login-page">
+      <div className="login-container">
+        <div className="login-image-section">
+          <img
+            src="/img/autos.jpg"
+            alt="Autos disponibles para alquiler"
+            className="login-image"
+          />
+        </div>
+        <div className="login-form-section">
+          <LoginForm />
+        </div>
+      </div>
     </div>
   );
 };
+
 export default Login;
