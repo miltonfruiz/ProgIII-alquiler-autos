ba844eb807bb1ddde7e176155429eaa87071795d
Milton Franco Ruiz <miltonfruizok@outlook.es>
2025-04-28 01:34:29 -0300

    Milton 28/04: Presentación estructura de Perfil

---
+++ src/pages/ProfileUser.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const ProfileUser = () => {
+  return (
+    <div>
+      <p>Componente Menu</p>
+      <p>Componente Perfil</p>
+      <p>Componente Mis Reservas</p>
+      <p>Componente Reservas pasadas</p>
+    </div>
+  );
+};
+
+export default ProfileUser;
