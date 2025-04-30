dfd55308ab88a72e79ca960bb8ef34146000fae5
Milton Franco Ruiz <miltonfruizok@outlook.es>
2025-04-29 21:20:28 -0300

    Milton 29/04: Creación componente Navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <div>
+        <img src="" alt="Logo" />
+      </div>
+      <div>
+        <input type="text" placeholder="Ingrese nombre o modelo de auto..." />
+        <button>Boton de Filtro</button>
+      </div>
+
+      <div>
+        <Link to="/autos">
+          <img src="" alt="autos" />
+        </Link>
+        <Link to="/perfil">
+          <img src="" alt="Perfil" />
+        </Link>
+      </div>
+    </nav>
+  );
+}
