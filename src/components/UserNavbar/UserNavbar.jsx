6a136975fa27e2624c2f079b840f8f93659bf4ab
Milton Franco Ruiz <miltonfruizok@outlook.es>
2025-04-30 01:04:54 -0300

    Milton 30/04: Creación de estilos a componente UserNavbar

--- src/components/UserNavbar/UserNavbar.jsx
+++ src/components/UserNavbar/UserNavbar.jsx
@@ -1,23 +1,46 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import "./UserNavbar.css";
 
 export default function Navbar() {
-  return (
-    <nav>
-      <div>
-        <img src="" alt="Logo" />
-      </div>
-      <div>
-        <input type="text" placeholder="Ingrese nombre o modelo de auto..." />
-        <button>Boton de Filtro</button>
-      </div>
+  const navigate = useNavigate();
 
-      <div>
-        <Link to="/autos">
-          <img src="" alt="autos" />
-        </Link>
-        <Link to="/perfil">
-          <img src="" alt="Perfil" />
-        </Link>
+  return (
+    <nav className="navbar">
+      <div className="navbar-left">
+        <div className="logo-wrapper">
+          <img
+            src=""
+            alt="Logo"
+            className="navbar-logo"
+            onClick={() => navigate("/testhome")}
+          />
+        </div>
+      </div>
+      <div className="navbar-center">
+        <div className="search-wrapper">
+          <input
+            type="text"
+            placeholder="Ingrese nombre o modelo de auto..."
+            className="navbar-search"
+          />
+          <button className="navbar-filter">
+            <img src="" alt="Filtro" />
+          </button>
+        </div>
+      </div>
+      <div className="navbar-right">
+        <div className="icon-wrapper">
+          <div className="cars-container">
+            <Link to="/cars">
+              <img src="" alt="Autos" className="navbar-icon" />
+            </Link>
+          </div>
+          <div className="user-container">
+            <Link to="/user-profile">
+              <img src="" alt="Perfil" className="navbar-icon" />
+            </Link>
+          </div>
+        </div>
       </div>
     </nav>
   );
