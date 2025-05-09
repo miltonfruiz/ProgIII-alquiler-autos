7368f4eef15ae8b220b86bffdb7d20a5f0528d9c
BrancoAntuña <118689753+BrancoMartin@users.noreply.github.com>
2025-05-09 16:10:41 -0300

    Branco: cambio en el diseño de carPayment

--- src/components/CarPayment/CarPayment.jsx
+++ src/components/CarPayment/CarPayment.jsx
@@ -1,8 +1,26 @@
 import React from "react";
 import Header from "../Header/Header";
 import { FaStar } from "react-icons/fa";
+import { RiVisaLine } from "react-icons/ri";
+import { FaCcMastercard } from "react-icons/fa6";
+import { SiAmericanexpress } from "react-icons/si";
+import { IoCard } from "react-icons/io5";
+import { useState } from "react";
 
 const CarPayment = () => {
+  const [imgTarjetas, setImgTarjetas] = useState("visa");
+
+  function handlerImagenes(e) {
+    if (e.target.value == "visa") {
+      setImgTarjetas("visa");
+    } else if (e.target.value == "mastercard") {
+      setImgTarjetas("mastercard");
+    } else if (e.target.value == "american") {
+      setImgTarjetas("american");
+    } else {
+      setImgTarjetas("debito");
+    }
+  }
   return (
     <div className="contenedor">
       <Header></Header>
@@ -10,51 +28,85 @@ const CarPayment = () => {
         <h2 className="titleDatos">Datos Facturacion</h2>
         <h3 className="subtituloDatos">introduce tus datos de facturacion</h3>
         <p className="paso1">Paso 1 de 3</p>
-        <label htmlFor="" className="labelDatos">
-          Nombre
-        </label>
-        <input type="text" className="inputDatos" />
-        <label htmlFor="" className="labelDatos">
-          Apellido
-        </label>
-        <input type="text" className="inputDatos" />
-        <label htmlFor="" className="labelDatos">
-          Numero de telefono
-        </label>
-        <input type="text" className="inputDatos" />
-        <label htmlFor="" className="labelDatos">
-          DNI
-        </label>
-        <input type="text" className="inputDatos" />
+        <div className="gridDatosFacturacion">
+          <div className="cajaInputDatos">
+            <label htmlFor="" className="labelDatos">
+              Nombre
+            </label>
+            <input type="text" className="inputDatos" placeholder="Nombre" />
+          </div>
+          <div className="cajaInputDatos">
+            <label htmlFor="" className="labelDatos">
+              Apellido
+            </label>
+            <input type="text" className="inputDatos" placeholder="Apellido" />
+          </div>
+          <div className="cajaInputDatos">
+            <label htmlFor="" className="labelDatos">
+              Numero de telefono
+            </label>
+            <input
+              type="text"
+              className="inputDatos"
+              placeholder="5493333333333"
+            />
+          </div>
+          <div className="cajaInputDatos">
+            <label htmlFor="" className="labelDatos">
+              DNI
+            </label>
+            <input type="text" className="inputDatos" placeholder="4000000" />
+          </div>
+        </div>
       </div>
       <div className="metodoPago">
         <h2 className="tituloMetodo">Metodo de Pago</h2>
         <h3 className="subtituloMetodo">Introduce tu metodo de pago</h3>
         <p className="paso2">Paso 2 de 3</p>
         <div className="ingresoTarjeta">
-          <select name="" id="" className="selectMetodo">
-            <option value="">Seleccione una opcion</option>
-            <option value="opcion 1">Visa</option>
-            <opcion value="opcion 2">Mastercard</opcion>
-            <opcion value="opcion 3">American express</opcion>
-            <opcion value="opcion 4">Tarjeta de debito</opcion>
+          <label htmlFor="seleccion">Seleccione una opcion</label>
+          <select
+            name=""
+            id="seleccion"
+            className="selectMetodo"
+            onChange={handlerImagenes}
+          >
+            <option value="visa">Visa</option>
+            <option value="mastercard">Mastercard</option>
+            <option value="american">American express</option>
+            <option value="debito">Tarjeta de debito</option>
           </select>
-          <label htmlFor="" className="labelMetodo">
-            Nombre de Tarjeta
-          </label>
-          <input type="text" className="inputMetodo" />
-          <label htmlFor="" className="labelMetodo">
-            Fecha de Expiracion
-          </label>
-          <input type="date" className="inputMetodo" />
-          <label htmlFor="" className="labelMetodo">
-            Nombre del titular
-          </label>
-          <input type="text" className="inputMetodo" />
-          <label htmlFor="" className="labelMetodo">
-            CVC
-          </label>
-          <input type="text" className="inputMetodo" />
+          {imgTarjetas == "visa" && <RiVisaLine />}
+          {imgTarjetas == "mastercard" && <FaCcMastercard />}
+          {imgTarjetas == "american" && <SiAmericanexpress />}
+          {imgTarjetas == "debito" && <IoCard />}
+
+          <div className="gridDatosTarjeta">
+            <div className="cajaInputTarjeta">
+              <label htmlFor="" className="labelMetodo">
+                Nombre de Tarjeta
+              </label>
+              <input type="text" className="inputMetodo" />
+            </div>
+            <div className="cajaInputTarjeta">
+              <label htmlFor="" className="labelMetodo">
+                Fecha de Expiracion
+              </label>
+              <input type="date" className="inputMetodo" />
+            </div>
+            <div className="cajaInputTarjeta">
+              <label htmlFor="" className="labelMetodo">
+                Nombre del titular
+              </label>
+              <input type="text" className="inputMetodo" />
+            </div>
+            <div className="cajaInputTarjeta">
+              <label htmlFor="" className="labelMetodo">
+                CVC
+              </label>
+              <input type="text" className="inputMetodo" />
+            </div>
+          </div>
         </div>
         <div className="cajaTransferenciaBancaria">
           <p className="transferenciaBancaria">Transferencia Bancaria</p>
@@ -98,8 +150,10 @@ const CarPayment = () => {
             <li className="liRequisitos">Ser mayor de 21 años.</li>
           </ul>
         </div>
-        <input type="checkbox" className="aceptoTerminos" />{" "}
-        <p className="textoAcepto">Acepto Terminos y condiciones</p>
+        <div className="cajaAceptoTerminos">
+          <input type="checkbox" className="aceptoTerminos" />
+          <p className="textoAcepto">Acepto Terminos y condiciones</p>
+        </div>
       </div>
       <aside>
         <div className="resumenDeAlquiler">
