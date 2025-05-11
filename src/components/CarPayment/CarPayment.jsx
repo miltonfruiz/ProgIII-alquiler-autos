9b46961b930eda22ca512e9abad16c1efbaa8aaf
Branco <brancoantu9@gmail.com>
2025-05-10 21:09:38 -0300

    Branco 10-5: acomodado de detalles del carPayment

--- src/components/CarPayment/CarPayment.jsx
+++ src/components/CarPayment/CarPayment.jsx
@@ -27,7 +27,7 @@ const CarPayment = () => {
       <div className="contenedor">
         <div className="contenedorIzquierda">
           <div className="DatosFacturacion">
-            <h2 className="titleDatos">Datos Facturacion</h2>
+            <h2 className="titleDatos">Datos de facturacion</h2>
             <h3 className="subtituloDatos">
               introduce tus datos de facturacion
             </h3>
@@ -80,7 +80,9 @@ const CarPayment = () => {
             <h3 className="subtituloMetodo">Introduce tu metodo de pago</h3>
             <p className="paso2">Paso 2 de 3</p>
             <div className="ingresoTarjeta">
-              <label htmlFor="seleccion">Seleccione una opcion</label>
+              <label htmlFor="seleccion" className="seleccion">
+                Seleccione una tarjeta
+              </label>
               <select
                 name=""
                 id="seleccion"
@@ -92,10 +94,14 @@ const CarPayment = () => {
                 <option value="american">American express</option>
                 <option value="debito">Tarjeta de debito</option>
               </select>
-              {imgTarjetas == "visa" && <RiVisaLine />}
-              {imgTarjetas == "mastercard" && <FaCcMastercard />}
-              {imgTarjetas == "american" && <SiAmericanexpress />}
-              {imgTarjetas == "debito" && <IoCard />}
+              {imgTarjetas == "visa" && <RiVisaLine className="logosTarjeta" />}
+              {imgTarjetas == "mastercard" && (
+                <FaCcMastercard className="logosTarjeta" />
+              )}
+              {imgTarjetas == "american" && (
+                <SiAmericanexpress className="logosTarjeta" />
+              )}
+              {imgTarjetas == "debito" && <IoCard className="logosTarjeta" />}
 
               <div className="gridDatosTarjeta">
                 <div className="cajaInputTarjeta">
@@ -194,31 +200,44 @@ const CarPayment = () => {
               Los precios pueden variar dependiendo de la duracion del alquiler
               y del precio de su coche de alquiler.
             </h3>
-            <img
-              className="imagenResumen"
-              src="public\images\camionetaCarPayment.png"
-              alt=""
-            />
-            <p className="nombreAutos">Volkswagen T-Cross</p>
-            <div className="valoracionResumen">
-              <FaStar className="estrella" />
-              <FaStar className="estrella" />
-              <FaStar className="estrella" />
-              <FaStar className="estrella" />
-              <FaStar className="estrella" />
-              <p className="valoracion">valoracion</p>
+            <div className="cajaValoracionAuto">
+              <img
+                className="imagenResumen"
+                src="public\images\camionetaCarPayment.png"
+                alt=""
+              />
+              <div className="cajaExtrellasNombre">
+                <p className="nombreAutos">Volkswagen T-Cross</p>
+                <div className="valoracionResumen">
+                  <FaStar className="estrella" />
+                  <FaStar className="estrella" />
+                  <FaStar className="estrella" />
+                  <FaStar className="estrella" />
+                  <FaStar className="estrella" />
+                  <p className="valoracion">valoracion</p>
+                </div>
+              </div>
             </div>
-            <div className="linea"></div>
 
-            <p className="tituloSubtotal">Subtotal</p>
-            <p className="subtotal">$290.000</p>
-            <p className="tituloImpuestos">Impuestos</p>
-            <p className="impuestos">$34.000</p>
-            <h2 className="tituloPrecioFinal">Precio final de renta</h2>
-            <h3 className="subtituloPrecioFinal">
-              Precio total con impuestos obligatorios
-            </h3>
-            <p className="precioFinal">$324.000</p>
+            <div className="linea"></div>
+            <div className="cajaSubtotal">
+              <p className="tituloSubtotal">Subtotal</p>
+              <p className="subtotal">$290.000</p>
+            </div>
+            <div className="cajaImpuestos">
+              <p className="tituloImpuestos">Impuestos</p>
+              <p className="impuestos">$34.000</p>
+            </div>
+            <div className="cajaPrecioFinal">
+              <div>
+                <h2 className="tituloPrecioFinal">Precio final de renta</h2>
+                <h3 className="subtituloPrecioFinal">
+                  Precio total con impuestos obligatorios
+                </h3>
+              </div>
+
+              <p className="precioFinal">$324.000</p>
+            </div>
           </div>
         </aside>
       </div>
