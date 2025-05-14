d83510b58816c600fafef350568bf6780d77e1f5
Branco <brancoantu9@gmail.com>
2025-05-14 12:26:15 -0300

    Branco 14-5: agregado de estilo de Landing Page

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -9,37 +9,56 @@ import { IoLogoWhatsapp } from "react-icons/io";
 const LandingPage = () => {
   return (
     <div>
-      <h1 className="titleLanding">Alquileres de autos</h1>
-      <Link to="https://www.instagram.com/">
-        <FaInstagram />
-      </Link>
-      <Link to="https://x.com/explore">
-        <FaTwitter />
-      </Link>
-      <Link to="https://www.facebook.com/?locale=es_ES">
-        <FaFacebook />
-      </Link>
-      <Link to="https://web.whatsapp.com/">
-        <IoLogoWhatsapp />
-      </Link>
+      <div className="container">
+        <div className="containerH1">
+          <h1 className="titleLanding">Alquileres de autos</h1>
+        </div>
+        <div className="containerSocialP">
+          <div className="containerSocial">
+            <Link to="https://www.instagram.com/">
+              <FaInstagram />
+            </Link>
+            <Link to="https://x.com/explore">
+              <FaTwitter />
+            </Link>
+            <Link to="https://www.facebook.com/?locale=es_ES">
+              <FaFacebook />
+            </Link>
+            <Link to="https://web.whatsapp.com/">
+              <IoLogoWhatsapp />
+            </Link>
+          </div>
+          <div className="containerP">
+            <p className="textWelcome">
+              Bienvenidos a la pagina de Alquileres de autos. Aqui podras
+              encontrar el auto que desees a un precio de alquiler accesible
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Accusantium rem voluptatem reiciendis nesciunt quaerat, deleniti
+              delectus dolore aperiam praesentium pariatur, eius aut consequatur
+              ut saepe consequuntur. Deserunt, asperiores? Maiores, atque. Lorem
+              ipsum dolor sit amet consectetur adipisicing elit. A, voluptatum
+              quas. Blanditiis necessitatibus pariatur dolorem amet dolorum
+              delectus quaerat. Rem maiores quisquam dolores ipsa incidunt autem
+              esse blanditiis minus laboriosam!
+            </p>
+          </div>
+        </div>
 
-      <p className="textWelcome">
-        Bienvenidos a la pagina de Alquileres de autos. Aqui podras encontrar el
-        auto que desees a un precio de alquiler accesible Lorem ipsum dolor sit
-        amet consectetur adipisicing elit. Accusantium rem voluptatem reiciendis
-        nesciunt quaerat, deleniti delectus dolore aperiam praesentium pariatur,
-        eius aut consequatur ut saepe consequuntur. Deserunt, asperiores?
-        Maiores, atque. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        A, voluptatum quas. Blanditiis necessitatibus pariatur dolorem amet
-        dolorum delectus quaerat. Rem maiores quisquam dolores ipsa incidunt
-        autem esse blanditiis minus laboriosam!
-      </p>
-      <button className="buttonRegisterLanding">Registrarse</button>
-      <button className="buttonLogin">Inicio Sección</button>
-      <p className="textDesign">
-        Diseñado por: Milton Ruiz, Juan Sosa ,Branco Antuña
-      </p>
-      <p className="copyright">© 2025 Todos los derechos reservados</p>
+        <div className="containerButton">
+          <button className="buttonRegisterLanding">Registrarse</button>
+          <button className="buttonLogin">Inicio Sección</button>
+        </div>
+
+        <div className="containerDesign">
+          <p className="textDesign">
+            Diseñado por: Milton Ruiz, Juan Sosa ,Branco Antuña
+          </p>
+        </div>
+
+        <div className="containerCopyright">
+          <p className="copyright">© 2025 Todos los derechos reservados</p>
+        </div>
+      </div>
     </div>
   );
 };
