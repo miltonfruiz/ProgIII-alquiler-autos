f2500d5ee1ff11b2b539a222b894c5317c0c0132
Branco <brancoantu9@gmail.com>
2025-05-14 10:59:58 -0300

    Branco 14-5: estructura de componente Landing Page

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 
 const LandingPage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1>Alquileres de autos</h1>
+      <p>
+        Bienvenidos a la pagina de Alquileres de autos. Aqui podras encontrar el
+        auto que desees a un precio de alquiler accesible Lorem ipsum dolor sit
+        amet consectetur adipisicing elit. Accusantium rem voluptatem reiciendis
+        nesciunt quaerat, deleniti delectus dolore aperiam praesentium pariatur,
+        eius aut consequatur ut saepe consequuntur. Deserunt, asperiores?
+        Maiores, atque. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        A, voluptatum quas. Blanditiis necessitatibus pariatur dolorem amet
+        dolorum delectus quaerat. Rem maiores quisquam dolores ipsa incidunt
+        autem esse blanditiis minus laboriosam!
+      </p>
+      <button>Registrarse</button>
+      <button>Inicio Sección</button>
+
+      <p>Diseñado por: Milton Ruiz, Juan Sosa ,Branco Antuña</p>
+      <p>© 2025 Todos los derechos reservados</p>
+    </div>
+  );
 };
 
 export default LandingPage;
