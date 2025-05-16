8ec4b9cedc3202da450340ef778313c36863f87e
Juan Sosa <juanmasosa162@gmail.com>
2025-05-16 16:47:32 -0300

    Cree componente overlay para el shop, falta agregar

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home/Home";
 import Shop from "./pages/Shop/Shop";
+import Overlay from "./components/Overlay/Overlay";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/home" element={<Home />} />
         <Route path="/shop" element={<Shop />} />
+        <Route path="/overlay" element={<Overlay />} />
       </Routes>
     </Router>
   );
