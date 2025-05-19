fda1bd4d215176826b8b9a1065b1b57d623b67f0
Milton Franco Ruiz <miltonfruizok@outlook.es>
2025-05-18 23:55:59 -0300

    Milton 18/05: Creación de lista en componente MyReservations

--- src/components/MyReservations/MyReservations.jsx
+++ src/components/MyReservations/MyReservations.jsx
@@ -24,6 +24,24 @@ export default function MyReservations() {
       fecha: "2025-07-01",
       imagen: "/images/volkswagen.png",
     },
+    {
+      id: 4,
+      auto: "Toyota Corolla",
+      fecha: "2025-06-12",
+      imagen: "/images/volkswagen.png",
+    },
+    {
+      id: 5,
+      auto: "Toyota Corolla",
+      fecha: "2025-07-01",
+      imagen: "/images/volkswagen.png",
+    },
+    {
+      id: 6,
+      auto: "Toyota Corolla",
+      fecha: "2025-07-01",
+      imagen: "/images/volkswagen.png",
+    },
   ]);
   const handleDelete = (id) => {
     const nuevasReservas = reservations.filter((res) => res.id !== id);
@@ -35,32 +53,35 @@ export default function MyReservations() {
         <FaCarSide className="car-myreservations" />
         Mis Reservas
       </h1>
-      <div className="reservation-list">
-        {reservations.length === 0 ? (
-          <p className="no-reservations">No hay reservas activas.</p>
-        ) : (
-          reservations.map((res) => (
-            <div
-              key={res.id}
-              className="reservation-card myreservations-fade-in"
-            >
-              <img src={res.imagen} alt={res.auto} className="car-image" />
-              <div className="reservation-info">
-                <h2>{res.auto}</h2>
-                <p>
-                  <MdDateRange className="data-myreservations" />
-                  Fecha: {new Date(res.fecha).toLocaleDateString("es-AR")}
-                </p>
-              </div>
-              <button
-                className="delete-myreservations"
-                onClick={() => handleDelete(res.id)}
+
+      <div className="reservation-scroll-wrapper">
+        <div className="reservation-list">
+          {reservations.length === 0 ? (
+            <p className="no-reservations">No hay reservas activas.</p>
+          ) : (
+            reservations.map((res) => (
+              <div
+                key={res.id}
+                className="reservation-card myreservations-fade-in"
               >
-                <RiDeleteBin6Line /> Cancelar
-              </button>
-            </div>
-          ))
-        )}
+                <img src={res.imagen} alt={res.auto} className="car-image" />
+                <div className="reservation-info">
+                  <h2>{res.auto}</h2>
+                  <p>
+                    <MdDateRange className="data-myreservations" />
+                    Fecha: {new Date(res.fecha).toLocaleDateString("es-AR")}
+                  </p>
+                </div>
+                <button
+                  className="delete-myreservations"
+                  onClick={() => handleDelete(res.id)}
+                >
+                  <RiDeleteBin6Line /> Cancelar
+                </button>
+              </div>
+            ))
+          )}
+        </div>
       </div>
     </div>
   );
