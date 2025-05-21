ef5af81678367c6b10e05dac1d0c11999d3ddbb5
Branco <brancoantu9@gmail.com>
2025-05-21 13:26:18 -0300

    Branco 21-5: sigo sin poder solucionar el problema del ref

--- src/components/CarPaymentValidation/CarPaymentValidation.jsx
+++ src/components/CarPaymentValidation/CarPaymentValidation.jsx
@@ -1,15 +1,15 @@
 const CarPaymentValidation = ({ datosFacturacion }) => {
   errores = {};
 
-  if (!datosFacturacion.nombre) {
+  if (!datosFacturacion.nombre?.trim()) {
     errores.nombre = "* debe ingresar el nombre";
   }
 
-  if (!datosFacturacion.apellido) {
+  if (!datosFacturacion.apellido?.trim()) {
     errores.apellido = "* debe ingresar el apellido";
   }
 
-  if (!datosFacturacion.numeroTelefonico) {
+  if (!datosFacturacion.numeroTelefonico?.trim()) {
     errores.numeroTelefonico = "* debe ingresar su numero telefonico";
   } else if (
     /!^(?:\+?\d{1,3}[-.\s]?)?(?:\(?\d{2,3}\)?[-.\s]?)?\d{4,}[-.\s]?\d{4}$/.test(
@@ -19,7 +19,7 @@ const CarPaymentValidation = ({ datosFacturacion }) => {
     errores.numeroTelefonico = "* ingrese un numero valido";
   }
 
-  if (!datosFacturacion.dni) {
+  if (!datosFacturacion?.dni.trim()) {
     errores.dni = "* debe ingresar su dni";
   } else if (!/^(?![0]+$)[0-9]{6,8}$/.test(datos.dni)) {
     errores.dni = "* ingrese un dni valido";
