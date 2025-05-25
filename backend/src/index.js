88f18594b61abe84743f5847a4e7488155f85098
Milton Franco Ruiz <miltonfruizok@outlook.es>
2025-05-25 19:28:48 -0300

    Main 25/05: Agregado de mejoras archivo index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,20 @@
 import express from "express";
-
-import { PORT } from "./config.js";
+import { PORT, sequelize } from "./config.js";
 import carsRoutes from "../routes/cars.routes.js";
-
+import { Car } from "./models/Car.js";
 const app = express();
 
-app.listen(PORT);
-app.use(carsRoutes);
-
-console.log(`Escuchando puerto ${PORT}`);
-console.log(`Conectado al puerto correctamente!`);
+app.use(express.json());
+app.use("/api", carsRoutes);
+async function startServer() {
+  try {
+    await sequelize.sync({ force: false });
+    console.log("Base de datos conectada y sincronizada!");
+    app.listen(PORT, () => {
+      console.log(`Servidor escuchando en el puerto ${PORT}...`);
+    });
+  } catch (error) {
+    console.error("Error al conectar con la base de datos:", error);
+  }
+}
+startServer();
