0c0d84cd0e926643739f6a2e337bb00ee1b3f648
Milton Franco Ruiz <miltonfruizok@outlook.es>
2025-05-25 19:10:32 -0300

    Main 25/05: Creación modelo cars

---
+++ backend/src/models/cars.js
@@ -0,0 +1,64 @@
+import { DataTypes } from "sequelize";
+import { sequelize } from "../config.js";
+
+export const Car = sequelize.define(
+  "Car",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    category: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    passengers: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    transmission: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+    },
+    brand: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    date: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+    },
+    tax: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+    },
+    paymentMethod: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    billing: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    total: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
