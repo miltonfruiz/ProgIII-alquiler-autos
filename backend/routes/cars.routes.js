370686dd6660d731149ef98198931d8968a73e93
Milton Franco Ruiz <miltonfruizok@outlook.es>
2025-05-25 19:02:09 -0300

    Main 22/05: Creación base de CRUD completo

--- backend/routes/cars.routes.js
+++ backend/routes/cars.routes.js
@@ -1,8 +1,29 @@
 import { Router } from "express";
-
 const router = Router();
 
 router.get("/cars", (req, res) => {
-  res.send("Obteniendo autos");
+  res.send("Obteniendo todos los autos");
 });
+
+router.get("/cars/:id", (req, res) => {
+  const { id } = req.params;
+  res.send(`Obteniendo auto con id: ${id}`);
+});
+
+router.post("/cars", (req, res) => {
+  const newCar = req.body;
+  res.send(`Creando un nuevo auto: ${JSON.stringify(newCar)}`);
+});
+
+router.put("/cars/:id", (req, res) => {
+  const { id } = req.params;
+  const updatedCar = req.body;
+  res.send(`Actualizando auto con id ${id}: ${JSON.stringify(updatedCar)}`);
+});
+
+router.delete("/cars/:id", (req, res) => {
+  const { id } = req.params;
+  res.send(`Eliminando auto con id: ${id}`);
+});
+
 export default router;
