0108f168f92772634e5145431fcde14c1189bab1
Milton Franco Ruiz <miltonfruizok@outlook.es>
2025-05-25 19:12:17 -0300

    Main 25/05: Agregado de mejoras archivo config.js

--- backend/src/config.js
+++ backend/src/config.js
@@ -1,1 +1,14 @@
+import { Sequelize } from "sequelize";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export const PORT = 3000;
+
+export const sequelize = new Sequelize({
+  dialect: "sqlite",
+  storage: path.join(__dirname, "../database.sqlite"),
+  logging: false,
+});
