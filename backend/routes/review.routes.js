0772622f5d6afc1bac0be37caad7b20256b2c4e0
Milton Franco Ruiz <miltonfruizok@outlook.es>
2025-05-26 01:21:23 -0300

    Main 26/05: Creación de modelo y ruta Review

---
+++ backend/routes/review.routes.js
@@ -0,0 +1,42 @@
+import { Router } from "express";
+import { Review } from "../src/models/Review.js";
+import { Car } from "../src/models/Car.js";
+
+const router = Router();
+
+//------------------- Crear comentario -------------------//
+router.post("/reviews", async (req, res) => {
+  try {
+    const { carId, username, rating, comment } = req.body;
+    const review = await Review.create({ carId, username, rating, comment });
+    const reviews = await Review.findAll({ where: { carId } });
+    const avgRating =
+      reviews.reduce((acc, r) => acc + r.rating, 0) / reviews.length;
+    await Car.update({ rating: avgRating }, { where: { id: carId } });
+    res.status(201).json(review);
+  } catch (error) {
+    res.status(500).json({ error: "Error al crear el comentario" });
+  }
+});
+//------------------- Obtener comentarios -------------------//
+router.get("/reviews/car/:carId", async (req, res) => {
+  try {
+    const { carId } = req.params;
+    const reviews = await Review.findAll({ where: { carId } });
+    res.json(reviews);
+  } catch (error) {
+    res.status(500).json({ error: "Error al obtener comentario" });
+  }
+});
+//------------------- Eliminar comentario -------------------//
+router.delete("/reviews/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleted = await Review.destroy({ where: { id } });
+    if (deleted) return res.sendStatus(204);
+    res.status(404).json({ error: "Comentario no encontrada" });
+  } catch (error) {
+    res.status(500).json({ error: "Error al eliminar el comentario" });
+  }
+});
+export default router;
--- backend/src/index.js
+++ backend/src/index.js
@@ -2,17 +2,21 @@ import express from "express";
 import { PORT } from "./config.js";
 import { sequelize } from "./db.js";
 import "./models/Car.js";
+import "./models/Review.js";
+import "./models/index.js";
 import carsRoutes from "../routes/cars.routes.js";
+import reviewsRoutes from "../routes/review.routes.js";
 
 const app = express();
+app.use(express.json());
 
 try {
-  app.listen(PORT);
   app.use(carsRoutes);
-
+  app.use(reviewsRoutes);
   await sequelize.sync();
-
-  console.log(`Server listening on port ${PORT}`);
+  app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+  });
 } catch (error) {
-  console.log(`There was an error on initialization`);
+  console.log(`There was an error on initialization:`, error);
 }
