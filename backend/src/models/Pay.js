bb0cf6a374e8deaaa3f611732fdea766376e2e66
Branco <brancoantu9@gmail.com>
2025-05-28 17:49:08 -0300

    Branco 28-5: creado del model PAY

---
+++ backend/src/models/Pay.js
@@ -0,0 +1,81 @@
+import { DataTypes } from "sequelize";
+import { sequelize } from "../db.js";
+
+export const Pay = sequelize.define("Pay", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  userId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: "User",
+      key: "id",
+    },
+  },
+  carId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: "Car",
+      key: "id",
+    },
+  },
+  reservationId: {
+    // esto depende del nombre que le pongamos a la tabla reservas si es diferente hay que cambiarlo
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: "Reservation",
+      key: "id",
+    },
+  },
+  subtotal: {
+    // esto es el precio del auto por dia multiplicado a los dias que se alquila osea (price de Car) * (dias_totales de Reservas)
+    type: DataTypes.FLOAT,
+    allowNull: false,
+  },
+  tax: {
+    // serian los impuestos
+    type: DataTypes.FLOAT,
+    allowNull: false,
+  },
+  total: {
+    // esto es el subtotal + el tax
+    type: DataTypes.FLOAT,
+    allowNull: false,
+  },
+  cardType: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  paymentMethod: {
+    type: DataTypes.ENUM("transferencia", "tarjeta"),
+    allowNull: false,
+  },
+  cardNumber: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  expirationDate: {
+    type: DataTypes.DATEONLY,
+    allowNull: true,
+  },
+  ownerName: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  cvc: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  voucher: {
+    type: DataTypes.STRING, // guardamos la ruta del archivo
+    allowNull: true,
+  },
+  acceptableTerms: DataTypes.BOOLEAN, // este es el checkbox de acepto terminos y condiciones
+  allowNull: false,
+  default: false,
+});
--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -1,11 +1,18 @@
 import { Car } from "./Car.js";
 import { Review } from "./Review.js";
 import { User } from "./User.js";
+import { Pay } from "./Pay.js";
 
 Car.hasMany(Review, { foreignKey: "carId", onDelete: "CASCADE" });
 Review.belongsTo(Car, { foreignKey: "carId" });
 
 User.hasMany(Review, { foreignKey: "userId", onDelete: "CASCADE" });
 Review.belongsTo(User, { foreignKey: "userId" });
+
+User.hasMany(Pay, { foreignKey: "userId", onDelete: "CASCADE" });
+Pay.belongsTo(User, { foreignKey: "userId", onDelete: "CASCADE" });
+
+User.hasMany(Car, { foreignKey: "carId", onDelete: "CASCADE" });
+User.belongsTo(User, { foreignKey: "carId", onDelete: "CASCADE" });
 
 export { Car, Review, User };
